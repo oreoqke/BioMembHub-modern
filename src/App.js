9a44f483da0f8d0dfe27bc7fc2cee265c17c086f
Alexey Kovalenko <2000ako@gmail.com>
2024-08-23 21:42:42 -0400

    rough draft

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes> {/* Use Routes instead of Switch */}
+          <Route path='/' element={<Home />} /> {/* Use element instead of component */}
+          <Route path='/services' element={<Services />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/sign-up' element={<SignUp />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
