2cf7da36554a6f61701a27b1eb7e5c5ac9565fd7
Alexey Kovalenko <2000ako@gmail.com>
2024-09-03 18:12:55 -0400

    adjusted spacing

--- src/components/pages/about/Team.js
+++ src/components/pages/about/Team.js
@@ -2,94 +2,86 @@ import React from 'react';
 import './Team.css';
 
 export function Team() {
+    const teamMembers = [
+        {
+            name: "Wonpil Im",
+            image: "images/team/Wonpil_Im.jpg",
+            role: "Professor of Biological Sciences",
+            department: "Bioengineering and Computer Sciences and Engineering",
+            university: "Lehigh University"
+        },
+        {
+            name: "Andrei Lomize",
+            image: "images/team/Andrei_Lomize.jpg",
+            role: "Research Scientist",
+            department: "Department of Medicinal Chemistry",
+            university: "University of Michigan"
+        },
+        {
+            name: "Alexey Kovalenko",
+            image: "images/team/Alexey_Kovalenko.jpg",
+            role: "Undergraduate Student",
+            department: "College of Literature, Science, and the Arts",
+            university: "University of Michigan"
+        },
+        {
+            name: "L Ponoop Prasad Patro",
+            image: "images/team/L_Ponoop_Prasad_Patro.jpg",
+            role: "Postdoctoral Fellow",
+            department: "Biological Sciences",
+            university: "Lehigh University"
+        },
+        {
+            name: "Sang-Jun Park",
+            image: "images/team/Sang-Jun_Park.jpg",
+            role: "Graduate Student",
+            department: "Computer Sciences and Engineering",
+            university: "Lehigh University"
+        },
+        {
+            name: "Zigang Song",
+            image: "images/team/Zigang_Song.jpg",
+            role: "Graduate Student",
+            department: "Biological Sciences",
+            university: "Lehigh University"
+        },
+        {
+            name: "Stanislav Cherepanov",
+            image: "images/team/Stanislav_Cherepanov.jpg",
+            role: "Graduate Student",
+            department: "Biophysics Program",
+            university: "University of Michigan"
+        },
+        {
+            name: "Yongsu Back",
+            image: "images/team/Yongsu_Back.jpg",
+            role: "Scientist",
+            company: "MolCube Inc."
+        },
+        {
+            name: "Jungyong Ji",
+            image: "images/team/Jungyong_Ji.jpg",
+            role: "Junior Scientist",
+            company: "MolCube Inc."
+        }
+    ];
+
     return (
         <>
             <div className='row'>
-                <div className='column'>
-                    <div className='container'>
-                        <img src="images/team/Wonpil_Im.jpg" alt="Team Member" className="container-image"/>
-                        <h3> Wonpil Im </h3>
-                        <div className="italic-text"> Professor of Biological Sciences, 
-                            Bioengineering and Computer Sciences 
-                            and Engineering, Lehigh University
+                {teamMembers.map((member, index) => (
+                    <div className='column' key={index}>
+                        <div className='container'>
+                            <img src={member.image} alt={`Team Member ${member.name}`} className="container-image" />
+                            <h3>{member.name}</h3>
+                            <div className="italic-text">
+                                {member.role} <br />
+                                {member.department && <>{member.department} <br /></>}
+                                {member.university || member.company}
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className='column'>
-                    <div className="container">
-                        <img src="images/team/Andrei_Lomize.jpg" alt="Team Member" className="container-image" />
-                        <h3> Andrei Lomize</h3>
-                        <div className="italic-text"> 
-                            Research Scientist Department of Medicinal Chemistry, 
-                            University of Michigan 
-                        </div>
-                    </div>
-                </div>
-                <div className='column'>
-                    <div className="container">
-                        <img src="images/team/Alexey_Kovalenko.jpg" alt="Team Member" className="container-image" />
-                        <h3> Alexey Kovalenko</h3>
-                        <div className="italic-text"> Undergraduate Student College of Literature, 
-                            Science, and the Art, University of Michigan
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='column'>
-                    <div className='container'>
-                        <img src="images/team/L_Ponoop_Prasad_Patro.jpg" alt="Team Member" className="container-image"/>
-                        <h3> L Ponoop Prasad Patro </h3>
-                        <div className="italic-text"> Postdoctoral Fellow Biological Sciences, 
-                            Lehigh University
-                        </div>
-                    </div>
-                </div>
-                <div className='column'>
-                    <div className="container">
-                        <img src="images/team/Sang-Jun_Park.jpg" alt="Team Member" className="container-image" />
-                        <h3> Sang-Jun Park</h3>
-                        <div className="italic-text"> Graduate student Computer Sciences 
-                            and Engineering, Lehigh University
-                        </div>
-                    </div>
-                </div>
-                <div className='column'>
-                    <div className="container">
-                        <img src="images/team/Zigang_Song.jpg" alt="Team Member" className="container-image" />
-                        <h3> Zigang Song</h3>
-                        <div className="italic-text"> Graduate Student Biological 
-                            sciences, Lehigh University
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className='row'>
-                <div className='column'>
-                    <div className='container'>
-                        <img src="images/team/Stanislav_Cherepanov.jpg" alt="Team Member" className="container-image"/>
-                        <h3> Stanislav Cherepanov </h3>
-                        <div className="italic-text"> Graduate student, 
-                            Department of Biophysics University of Michigan
-                        </div>
-                    </div>
-                </div>
-                <div className='column'>
-                    <div className="container">
-                        <img src="images/team/Yongsu_Back.jpg" alt="Team Member" className="container-image" />
-                        <h3> Yongsu Back</h3>
-                        <div className="italic-text"> Scientist, MolCube Inc.
-                        </div>
-                    </div>
-                </div>
-                <div className='column'>
-                    <div className="container">
-                        <img src="images/team/Jungyong_Ji.jpg" alt="Team Member" className="container-image" />
-                        <h3> Jungyong Ji</h3>
-                        <div className="italic-text"> Junior Scientist, MolCube Inc.
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </>
     );
