8046c175fd0c14e01cea72d5e23f1e79344f0440
Alexey Kovalenko <2000ako@gmail.com>
2024-09-12 23:53:03 -0400

    option to use cloud storage commented out

--- src/components/Assets.js
+++ src/components/Assets.js
@@ -2,35 +2,51 @@ import { Urls } from './Links';
 
 const { cloud_storage_prefix } = Urls;
 
+// Helper function to prepend cloud storage prefix to image paths
+// You can comment out this function if you are not using Google Cloud Storage
+const prependCloudPrefix = (paths) => {
+//   const result = {};
+//   for (const key in paths) {
+//     result[key] = cloud_storage_prefix + paths[key];
+//   }
+  return paths;
+};
+
+// Define image paths without prefix
+const imagePaths = {
+    home_background: "images/BioMembHub4.png",
+    architecture: "images/Architecture.png",
+    cellpm: "images/CELLPM-logo.jpg",
+    tmnet_server: "images/Logo_1TMnet_server.jpg",
+    BMH: "images/Logo_BMH.png",
+    FMAP_server: "images/Logo_FMAP_server.jpg",
+    MEMBRANOME: "images/Logo_MembranomeX_DB2.jpg",
+    oprlm_db: "images/Logo_OPRLM_DB2.jpg",
+    oprlm_server: "images/Logo_OPRLM_server.jpg",
+    permm_server: "images/Logo_PERMM_server1.jpg",
+    permm_db: "images/Logo_PerMM_DB2.jpg",
+    ppm: "images/Logo_PPM_server.jpg",
+    tmdock: "images/Logo_TMDOCK_server.jpg",
+    tmpfold: "images/Logo_TMPFOLD_server2.jpg",
+    nsf: "images/NSF_logo.jpg",
+    fmap: "images/Logo_FMAP_server.jpg",
+    lehigh_university: "images/Lehigh_University.jpg",
+};
+
+const teamPaths = {
+    alexey_kovalenko: "images/team/Alexey_Kovalenko.jpg",
+    andrei_lomize: "images/team/Andrei_Lomize.jpg",
+    jungyong_ji: "images/team/Jungyong_Ji.jpg",
+    l_ponoop_prasad_patro: "images/team/L_Ponoop_Prasad_Patro.jpg",
+    sang_jun_park: "images/team/Sang-Jun_Park.jpg",
+    stanislav_cherepanov: "images/team/Stanislav_Cherepanov.jpg",
+    wonpil_im: "images/team/Wonpil_Im.jpg",
+    yongsu_baek: "images/team/Yongsu_Baek.jpg",
+    zigang_song: "images/team/Zigang_Song.jpg",
+};
+
+// Apply cloud prefix to all images and team member images
 export const Assets = {
-    images: {
-        home_background: cloud_storage_prefix + "images/BioMembHub4.png",
-        architecture: cloud_storage_prefix + "images/Architecture.png",
-        cellpm: cloud_storage_prefix + "images/CELLPM-logo.jpg",
-        tmnet_server: cloud_storage_prefix + "images/Logo_1TMnet_server.jpg",
-        BMH: cloud_storage_prefix + "images/Logo_BMH.png",
-        FMAP_server: cloud_storage_prefix + "images/Logo_FMAP_server.jpg",
-        MEMBRANOME: cloud_storage_prefix + "images/Logo_MembranomeX_DB2.jpg",
-        oprlm_db: cloud_storage_prefix + "images/Logo_OPRLM_DB2.jpg",
-        oprlm_server: cloud_storage_prefix + "images/Logo_OPRLM_server.jpg",
-        permm_server: cloud_storage_prefix + "images/Logo_PERMM_server1.jpg",
-        permm_db: cloud_storage_prefix + "images/Logo_PerMM_DB2.jpg",
-        ppm: cloud_storage_prefix + "images/Logo_PPM_server.jpg",
-        tmdock: cloud_storage_prefix + "images/Logo_TMDOCK_server.jpg",
-        tmpfold: cloud_storage_prefix + "images/Logo_TMPFOLD_server2.jpg",
-        nsf: cloud_storage_prefix + "images/NSF_logo.jpg",
-        fmap: cloud_storage_prefix + "images/Logo_FMAP_server.jpg",
-        lehigh_university: cloud_storage_prefix + "images/Lehigh_University.jpg",
-    },
-    team: {
-        alexey_kovalenko: cloud_storage_prefix + "images/team/Alexey_Kovalenko.jpg",
-        andrei_lomize: cloud_storage_prefix + "images/team/Andrei_Lomize.jpg",
-        jungyong_ji: cloud_storage_prefix + "images/team/Jungyong_Ji.jpg",
-        l_ponoop_prasad_patro: cloud_storage_prefix + "images/team/L_Ponoop_Prasad_Patro.jpg",
-        sang_jun_park: cloud_storage_prefix + "images/team/Sang-Jun_Park.jpg",
-        stanislav_cherepanov: cloud_storage_prefix + "images/team/Stanislav_Cherepanov.jpg",
-        wonpil_im: cloud_storage_prefix + "images/team/Wonpil_Im.jpg",
-        yongsu_baek: cloud_storage_prefix + "images/team/Yongsu_Baek.jpg",
-        zigang_song: cloud_storage_prefix + "images/team/Zigang_Song.jpg",
-    }
+  images: prependCloudPrefix(imagePaths),
+  team: prependCloudPrefix(teamPaths),
 };
